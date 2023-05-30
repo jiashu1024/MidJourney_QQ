aecd5635524d0a330bb9d76b6071e6b818a4a3d5
zhangbinbin <1130600015@qq.com>
2023-05-30 23:30:00 +0800

    [fix] add bot offline bot event and wechat notify

--- src/main/java/com/zjs/mj/Bot/QQBot.java
+++ src/main/java/com/zjs/mj/Bot/QQBot.java
@@ -12,6 +12,7 @@ import net.mamoe.mirai.BotFactory;
 import net.mamoe.mirai.auth.BotAuthorization;
 import net.mamoe.mirai.event.Event;
 import net.mamoe.mirai.event.GlobalEventChannel;
+import net.mamoe.mirai.event.events.BotOfflineEvent;
 import net.mamoe.mirai.event.events.FriendMessageEvent;
 import net.mamoe.mirai.event.events.GroupMessageEvent;
 import net.mamoe.mirai.utils.BotConfiguration;
@@ -89,6 +90,10 @@ public class QQBot implements ApplicationListener<ContextRefreshedEvent> {
             }
             if (qqEvent instanceof FriendMessageEvent) {
                 friendMessageEventProcessor.process(qqEvent);
+            }
+
+            if (qqEvent instanceof BotOfflineEvent) {
+                WxBotService.sendText("bot["+ bot.getId() + "]掉线");
             }
         });
         try {
